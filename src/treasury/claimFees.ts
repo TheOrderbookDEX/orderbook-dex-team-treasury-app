f8408864316e4fbc95893be5768e75ab1c82c2f7
Frugal-Wizard <108354301+Frugal-Wizard@users.noreply.github.com>
2023-02-05 12:19:42 -0300

    add todo tag for gas estimate bug

--- src/treasury/claimFees.ts
+++ src/treasury/claimFees.ts
@@ -3,6 +3,8 @@ import { IOrderbookDEXTeamTreasury } from '@theorderbookdex/orderbook-dex-team-t
 import { SupportedChainId } from './SupportedChainId';
 import { treasuryAddress } from './treasuryAddress';
 
+// TODO fix wrong gas estimation
+
 export async function claimFees(chainId: SupportedChainId, orderbooks: Address[], abortSignal?: AbortSignal): Promise<void> {
   try {
     const treasury = IOrderbookDEXTeamTreasury.at(treasuryAddress[chainId]);
